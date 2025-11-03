a498144cc5672ecfe1c71e047041441d537139f8
José Zehentner <188351564+josezehentner@users.noreply.github.com>
2025-11-03 18:27:01 +0100

    Add streamlit doku

--- Docker/data-pipeline/data_utilities/database_connector.py
+++ Docker/data-pipeline/data_utilities/database_connector.py
@@ -1,4 +1,3 @@
-import psycopg2
 import os
 from sqlalchemy import create_engine
 
@@ -8,16 +7,6 @@ DB_USER = "appuser"
 DB_PASS = "group8"
 DB_PORT = 5432
 
-def get_psycopg2_connection():
-    """Returns a psycopg2 database connection."""
-    return psycopg2.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASS
-    )
 
 def get_sqlalchemy_engine():
-    """Returns a SQLAlchemy engine (useful for pandas.read_sql)."""
     return create_engine(f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
